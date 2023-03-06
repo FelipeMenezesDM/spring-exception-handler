dcdd6a57760eb001c070219b9ecf04654ffd1182
FelipeMenezesDM <contato@felipemenezes.com.br>
2023-03-05 23:13:43 -0300

    feat: implementando tratamento de exceções para validação com spring validation

---
+++ src/main/java/br/com/felipemenezesdm/dto/ExceptionDetailFieldDTO.java
@@ -0,0 +1,37 @@
+package br.com.felipemenezesdm.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ExceptionDetailFieldDTO {
+    private String field;
+
+    private String value;
+
+    private String message;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/br/com/felipemenezesdm/exception/ApiExceptionHandler.java
+++ src/main/java/br/com/felipemenezesdm/exception/ApiExceptionHandler.java
@@ -1,15 +1,28 @@
 package br.com.felipemenezesdm.exception;
 
+import br.com.felipemenezesdm.dto.ExceptionDetailFieldDTO;
+import br.com.felipemenezesdm.dto.ExceptionResponseDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
-
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import static java.time.Instant.now;
+import static java.time.ZoneId.systemDefault;
+import static java.time.format.DateTimeFormatter.ofPattern;
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
 @ControllerAdvice
@@ -17,6 +30,8 @@ import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 public class ApiExceptionHandler {
     @Autowired
     ObjectMapper objectMapper;
+
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
 
     @ExceptionHandler(BadGatewayException.class)
     protected ResponseEntity<Object> handler(BadGatewayException e) {
@@ -93,8 +108,53 @@ public class ApiExceptionHandler {
         return buildResponseEntity(e.getResponseBodyAsString(), e.getStatusCode());
     }
 
-    private ResponseEntity<Object> buildResponseEntity(Object payload, HttpStatus httpStatus) {
-        return new ResponseEntity<>(payload, httpStatus);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handler(MethodArgumentNotValidException e) {
+        return buildResponseEntity(e, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<Object> handler(ConstraintViolationException e, WebRequest request) {
+        return buildResponseEntity(e, HttpStatus.BAD_REQUEST, ((ServletWebRequest) request).getRequest());
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(Throwable e, HttpStatus httpStatus) {
+        return buildResponseEntity(e, httpStatus, null);
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(Throwable e, HttpStatus httpStatus, HttpServletRequest request) {
+        ExceptionResponseDTO dto = new ExceptionResponseDTO();
+        List<ExceptionDetailFieldDTO> details = new ArrayList<>();
+        HashMap<String, String> links = new HashMap<>();
+        StringBuilder endPoint = new StringBuilder();
+
+        if(!Objects.isNull(request)) {
+            String queryString = request.getQueryString().isEmpty() ? "" : ("?").concat(request.getQueryString());
+
+            endPoint.append(request.getRequestURI());
+            links.put("self", request.getRequestURL().toString().concat(queryString));
+
+            if(e instanceof ConstraintViolationException) {
+                ((ConstraintViolationException) e).getConstraintViolations().forEach((violation) -> {
+                    ExceptionDetailFieldDTO field = new ExceptionDetailFieldDTO();
+                    field.setField(violation.getPropertyPath().toString());
+                    field.setValue(violation.getInvalidValue().toString());
+                    field.setMessage(violation.getMessage());
+
+                    details.add(field);
+                });
+            }
+        }
+
+        dto.setPath(endPoint.toString());
+        dto.setDetails(details);
+        dto.setLinks(links);
+        dto.setStatus(httpStatus.value());
+        dto.setError(httpStatus.name());
+        dto.setMessage(e.getMessage());
+        dto.setTimestamp(ofPattern(DATE_TIME_FORMAT).withZone(systemDefault()).format(now()));
+
+        return new ResponseEntity<>(dto, httpStatus);
     }
 
     private ResponseEntity<Object> buildResponseEntity(String payload, HttpStatus status) throws JsonProcessingException {
